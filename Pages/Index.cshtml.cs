58ed828691a24052a8c62ac0c017b9eaddb656e1
10Ngwako <ngoakoramakgoakgoa@gmail.com>
2026-01-14 11:47:22 +0200

    first commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SimpleHotelBooking.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
